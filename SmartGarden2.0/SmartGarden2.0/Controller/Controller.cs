a469414098fb8fa24379e73042727f5cf8f28a45
FraGio <giova6@hotmail.it>
2015-10-09 15:47:39 +0200

    SmartGarden2.0

---
+++ SmartGarden2.0/SmartGarden2.0/Controller/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGarden2._0.Controller
+{
+    class Controller
+    {
+        //private readonly GestioneGiardino;
+        public Controller(GestioneGiardino gestioneGiardino)
+        {
+
+        }
+    }
+}
---
+++ SmartGarden2.0/SmartGarden2.0/GestioneGiardino.cs
@@ -0,0 +1,6 @@
+﻿namespace SmartGarden2._0.Controller
+{
+    public class GestioneGiardino
+    {
+    }
+}
