a4226ee1774ec594b020c684ab8b0fabcabe92f9
FraGio <giova6@hotmail.it>
2015-10-10 09:48:32 +0200

    Versione completa con timer passivo

---
+++ SmartGarden2.0/SmartGarden2.0/MyTimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerPassivo
+{
+    class MyTimer
+    {
+        public IObservable<long> GetTimerPerValvola(int seconds)
+        {
+            var timer = Observable.Timer(TimeSpan.FromSeconds(seconds));
+            return timer;
+        }
+    }
+}
---
+++ SmartGarden2.0/SmartGarden2.0/Settore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerPassivo
+{
+    class Settore
+    {
+        Valvola valvola;
+        String name;
+
+        public String Nome
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                valvola = new Valvola();
+                valvola.Name = "valvola" + name;
+            }
+        }
+
+        internal void setTimer(IObservable<long> timer)
+        {
+            timer.Subscribe(valvola);
+        }
+    }
+}
