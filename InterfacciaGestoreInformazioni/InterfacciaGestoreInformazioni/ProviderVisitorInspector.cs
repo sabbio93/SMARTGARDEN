d3f6e63bbf836390a0464e6bc135c6984a1e25ad
sabbio93 <sabbio.a93@gmail.com>
2015-10-15 11:01:57 +0200

    gestore info 1.4

--- InterfacciaGestoreInformazioni/InterfacciaGestoreInformazioni/ProviderVisitorInspector.cs
+++ InterfacciaGestoreInformazioni/InterfacciaGestoreInformazioni/ProviderVisitorInspector.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using WindowsFormsApplication1.Fornitori;
+using WindowsFormsApplication1 ;
 
 namespace WindowsFormsApplication1
 {
